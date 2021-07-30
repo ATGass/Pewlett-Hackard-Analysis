4b9258dfb685015e978e3f6444ed12df27a5d148
ATGass <atgass25@gmail.com>
2021-07-29 23:17:10 -0500

    Challenge Update

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,8 @@
 -- Starter, import into retirement_titles
 SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
 INTO retirement_titles
-FROM employees as e
+FROM current_emp as e
 JOIN titles as ti USING(emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no
 ;
 
